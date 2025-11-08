b46ef8c82395e4f680c9467224c8c89b146dbd6a
مصعب بن فهد بن صالح بن محمد <grengohn@gmail.com>
2025-11-08 12:42:46 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10159806
+    "zoneId": 10160064
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
